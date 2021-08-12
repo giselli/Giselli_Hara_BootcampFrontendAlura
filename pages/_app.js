b9d55f761afbd047f10570a7ff4543a6977140a1
Giselli Hara <gi@hara.ninja>
2021-08-12 17:03:00 -0300

    _app, index e GlobalStyle iniciais

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,28 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-};
+import React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/theme';
+import GlobalStyle from '../src/theme/GlobalStyle';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;700&display=swap'
+          rel='stylesheet'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Fira+Sans&display=swap'
+          rel='stylesheet'
+        />
+        <title>Giselli Hara -- Portfólio</title>
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  );
+  )
 }
